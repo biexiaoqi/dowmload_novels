523a1f50e78ba7c0683fab2ced9e9cbf5962cbd1
biexiaoqi <3116997431@qq.com>
2025-04-25 21:10:34 +0800

    Add files via upload
    主程序

---
+++ main.py
@@ -0,0 +1,20 @@
+from Pen_Fun_Pavilion import Pen_Fun_Pavilion_main
+import os
+
+
+filename = '小说.txt'
+def save_chapter(title, content, filename):
+    with open(filename, 'a', encoding='UTF-8') as f:
+        f.write(f"\n\n{title}\n\n")
+        # 使用空格分割内容
+        words = content.split()
+        for word in words:
+            f.write(word.strip() + '\n')
+
+
+if __name__ == '__main__':
+    url = input()
+
+    if os.path.exists(filename):
+        os.remove(filename)  # 如果文件已存在，先删除它
+    Pen_Fun_Pavilion_main(url)
